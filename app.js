7b21320c8c98a7ce6256430803b0312cb85971ff
IsoHovhannisyan <143291832+IsoHovhannisyan@users.noreply.github.com>
2024-01-19 02:15:35 +0400

    second commit

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
   origin: [
     'http://localhost:3000',
+    'https://blossom-belle-cosmetics.vercel.app',
   ],
   credentials: true
 }));
