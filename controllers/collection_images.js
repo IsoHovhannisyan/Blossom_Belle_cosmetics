fdb83da5dcca86df1de8df36a5f050c21795c3e7
IsoHovhannisyan <143291832+IsoHovhannisyan@users.noreply.github.com>
2024-03-02 19:40:16 +0400

    check upload message

--- controllers/collection_images.js
+++ controllers/collection_images.js
@@ -63,11 +63,11 @@ const add = async (req, res) => {
         }
 
         const imagePath = image.path;
-        const imageFileName = imagePath.slice(imagePath.lastIndexOf("\\") + 1);
+        const imageFileName1 = imagePath.slice(imagePath.lastIndexOf("\\") + 1);
+        const imageFileName = imageFileName1.slice(imageFileName1.lastIndexOf("/")+1);
         const updatedImageFileName = `${folder}_${Date.now()}_${imageFileName}`;
         const imageFullPath = path.join(nameFolder, updatedImageFileName);
         const imageURL = path.join("/images", folder, updatedImageFileName).replace(/\\/g, '/');
-        return res.status(500).json({ message: imageURL, imagePath, imageFullPath });
 
         fs.rename(imagePath, imageFullPath, (err) => {
             if (err) {
