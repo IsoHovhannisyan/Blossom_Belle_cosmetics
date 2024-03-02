1520f0b3421a1fabad1c1efaec93b9bc982de7d3
IsoHovhannisyan <143291832+IsoHovhannisyan@users.noreply.github.com>
2024-03-02 20:08:54 +0400

    check upload message

--- controllers/collection_images.js
+++ controllers/collection_images.js
@@ -65,11 +65,6 @@ const add = async (req, res) => {
         const updatedImageFileName = `${folder}_${Date.now()}_${imageFileName}`;
         const imageFullPath = path.join(nameFolder, updatedImageFileName);
         const imageURL = path.join("/images", folder, updatedImageFileName).replace(/\\/g, '/');
-        fs.rename(imagePath, '/var/task/public/images/slider/aaaa.jpg', (err) => {
-            if (err) {
-                return res.status(500).json({ message: err, imagePath, imageFullPath });
-            }
-        });
         fs.copyFile(imagePath, imageFullPath, async (err) => {
             if (err) {
                 return res.status(500).json({ message: "Failed to move the uploaded file", err });
