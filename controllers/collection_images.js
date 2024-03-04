86107e2f60de21a6e5c36728690bab1e6a93c8f2
IsoHovhannisyan <143291832+IsoHovhannisyan@users.noreply.github.com>
2024-03-05 02:18:47 +0400

    check upload message

--- controllers/collection_images.js
+++ controllers/collection_images.js
@@ -71,6 +71,7 @@ const add = async (req, res) => {
     // If does not have image mime type prevent from uploading
 
     // Move the uploaded image to our upload folder
+    return res.status(400).json({ message: imagePath,updatedImageFileName, });
     image.mv(`${nameFolder}` + '/' + updatedImageFileName + '.jpg');
 
     // All good
